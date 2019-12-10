61dd1cb1a73939af0a4028d5e97729cb08f16fcf
gekas920 <valeryanka77777@gmail.com>
2019-12-10 19:55:18 +0300

    Checking pass and send token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
   def login
     @user = User.find_by_login(params[:login])
-    render json: @user
+    if @user
+      if @user.password === params[:password]
+        render plain: gen_token(@user.id)
+      else
+        render plain: 'incorrect password'
+      end
+    else
+      render plain: 'incorrect data'
+    end
+  end
+  def gen_token(payload)
+    password = 'CRM_SYSTEM'
+    JWT.encode payload, password, 'none'
   end
 end
