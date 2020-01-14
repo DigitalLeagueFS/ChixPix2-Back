615e04d9587354ed2eb5e2528dcf837920e35d0e
gekas920 <valeryanka77777@gmail.com>
2020-01-14 13:40:44 +0300

    Create client

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,7 +4,22 @@ class ClientsController < ApplicationController
     render json: @client.as_json
   end
   def addClients
-    puts params
+    puts params[:body]
+    @company = Company.find_by_company_name(params[:clickedCompany])
+    @client = Client.create(
+        :name=>params[:name],
+        :surname=>params[:surname],
+        :thirdName=>params[:thirdName],
+        :phone=>params[:phone],
+        :date=>params[:date],
+        :link=>params[:link],
+        :company_id=>@company.id
+    )
+    if @client
+      render json: {status:'ok'}, status: 200
+    else
+      render json: {status:'not ok'}, status: 500 
+    end
   end
   def getClient
     @client = Client.joins(:company).select('name','surname','thirdName','phone','date','link','company_name').find(params[:id])
